c1811b406913d862689282fdad71ccac9c686b80
DoggoMedic <josegonzalezag379@gmail.com>
2025-10-20 21:46:10 -0300

    Comienzo de codigo con definiciones e comienzo para leer archivos que tendran las matrizes en txt

--- Tarea.c
+++ Tarea.c
@@ -1,4 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+#define MAX_VERTICES 12
+#define INFINITY __INT_MAX__
+
+int distancia[MAX_VERTICES];
+int predecesor[MAX_VERTICES];
+int visitado[MAX_VERTICES];
+
+int nodoAIndice (char c){
+    return c - 'a';
+}
+
+char indiceANodo (int i){
+    return i + 'a';
+}
+
+int leerArchivo(int matrizAdyacente[MAX_VERTICES][MAX_VERTICES], int *numNodos, const char *nombreArchivo, int orientacion){
+
+    for (int i=0; i<MAX_VERTICES; i++){
+        for(int j=0; j<MAX_VERTICES; j++){
+            matrizAdyacente[i][j]=0;
+        }
+    }
+
+    FILE *file = fopen(nombreArchivo, "r");
+    if (file == NULL){
+        printf("Error al abrir el archivo.\n");
+        return -1;
+    }
+}
 
 int main(){
     return 0;
